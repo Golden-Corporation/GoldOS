48d6ba0e8324b48ec73d6ec34484528bfd8c82fa
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-03-06 10:58:46 +0800

    Added Install.py

---
+++ Install.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM')
+
+os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/BG.png')
