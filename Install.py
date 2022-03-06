c094cfa49026f6dc75cb4e376b82419769534eea
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-03-06 14:25:53 +0800

    Update Install.py
    Updated it too add extra stuffs :D

--- Install.py
+++ Install.py
@@ -1,5 +1,15 @@
 import os
+import time
 
+input('Start installing? ')
+print('Installing.')
+print('Setup may require for you to enter your password a few times.')
+time.sleep(5)
+
+os.system('sudo apt install plymouth-themes')
 os.system('gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM')
-
 os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/BG.png')
+
+time.sleep(2.5)
+
+os.system()
