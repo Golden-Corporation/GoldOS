46f2ce6f9e2213e416562277c58bd6f5c350a127
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-03-11 18:11:17 +0800

    CUSTOM BOOT?!?!
    yes yes yes yay

--- Install.py
+++ Install.py
@@ -12,4 +12,11 @@ os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/BG.p
 
 time.sleep(2.5)
 
-os.system()
+os.system('sudo mv $PWD/NewLogo.png /usr/share/plymouth/themes/spinner/watermark.png')
+os.system('sudo mv $PWD/GoldOSMiniLogo.png /user/share/plymouth/themes/spinner/bgrt-fallback.png')
+
+print('Install complete! Your system will reboot in 5 seconds.')
+
+time.sleep(5)
+
+os.system('sudo reboot')
