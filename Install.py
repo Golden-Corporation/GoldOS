2678676c32ac94911876ddce9c7c2ce626fb6b1a
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-03-19 15:00:57 +0800

    Trimmed to only installing the necessary modules

--- Install.py
+++ Install.py
@@ -3,25 +3,9 @@ import time
 
 input('Start installing? ')
 print('Installing.')
-print('Setup may require for you to enter your password a few times.')
+print('Setup will restart your computer later into the installation.')
 
 os.system('sudo apt install plymouth-themes')
-os.system('gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM')
-os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/BG.png')
+os.system('sudo apt install python3-tk')
 
-time.sleep(2.5)
-
-os.system('sudo cp $PWD/NewLogo.png /usr/share/plymouth/themes/spinner/watermark.png')
-
-try:
-    os.system('sudo cp $PWD/GoldOSMiniLogo.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png')
-finally:
-    pass
-
-os.system('sudo cp $PWD/OSData.txt /usr/lib/os-release')
-
-print('Install complete! Your system will reboot in 5 seconds.')
-
-time.sleep(5)
-
-os.system('sudo reboot')
+os.system('python3 Start2.py')
