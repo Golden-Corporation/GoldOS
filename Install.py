032ed1f4af2e8ce9ed0d2064802ef360dfcfbb0d
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-03-23 16:06:18 +0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -7,3 +7,5 @@ print('Setup will restart your computer later into the installation.')
 
 os.system('sudo apt install plymouth-themes')
 os.system('sudo apt install python3-tk')
+
+os.system('python3 Start2.py')
