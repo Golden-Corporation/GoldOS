d7dcd2205dad77a96abac6c4d7594d702a0bf824
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-04-21 20:12:38 +0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -3,9 +3,16 @@ import time
 
 input('Start installing? ')
 print('Installing.')
-print('Setup will restart your computer later into the installation.')
+print('Please wait for a little while, while we prepare.')
 
-os.system('sudo apt install plymouth-themes')
-os.system('sudo apt install python3-tk')
+time.sleep(3)
+
+os.system('sudo apt-get update') # Always run this first, can fix broken installs.
+
+os.system('sudo apt -y install plymouth-themes')
+os.system('sudo apt -y install python3-tk')
+os.system('sudo apt -y install git')
+
+print('\nFinishing up.')
 
 os.system('python3 Start2.py')
