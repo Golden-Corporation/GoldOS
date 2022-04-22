a31317a77b005b9e0a51f28697026e3c0cc3d06b
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-04-22 16:22:19 +0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -7,6 +7,11 @@ print('Please wait for a little while, while we prepare.')
 
 time.sleep(3)
 
+# Next lines are to stop update locks from working if the user has the program running.
+os.system('sudo killall -9 synaptic')
+os.system('sudo rm /var/lib/dpkg/lock-frontend')
+os.system('sudo rm /var/lib/dpkg/lock')
+
 os.system('sudo apt-get update') # Always run this first, can fix broken installs.
 
 os.system('sudo apt -y install plymouth-themes')
