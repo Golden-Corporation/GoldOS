5cb4145dcdca37de2e449987caa99bd84e2e50bb
Coder2354 <80308744+Coder2354@users.noreply.github.com>
2022-05-09 17:06:38 +0800

    Update Start2.py

--- Start2.py
+++ Start2.py
@@ -1,35 +1,36 @@
 import os
 from tkinter import *
 
-os.system('sudo cp $PWD/VerNum.txt /var/cache/Gversion.txt')
 os.system('sudo cp $PWD/Assets/NewLogo.png /usr/share/plymouth/themes/spinner/watermark.png')
 os.system('sudo cp $PWD/Assets/GoldOSMiniLogo.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png')
 os.system('sudo cp $PWD/Assets/GoldOSMiniLogo.png /usr/share/pixmaps/ubuntu-logo-icon.png')
 os.system('sudo cp $PWD/OSData.txt /etc/os-release')
 os.system('gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM')
-os.system('sudo cp $PWD/Assets/NewLogo.png /usr/share/pixmaps/ubuntu-logo-dark')
-os.system('sudo cp $PWD/Assets/NewLogo.png /usr/share/pixmaps/ubuntu-logo')
+os.system('sudo cp $PWD/Assets/NewLogo.png /usr/share/plymouth/themes/spinner/watermark.png')
+os.system('sudo cp $PWD/OSData.txt /usr/lib/os-release')
+os.system('sudo cp $PWN/Assets/NewLogo.png /usr/share/pixmaps/ubuntu-logo')
 
 window = Tk() # Only run when all other install processes are done.
 
 def Clean():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/CleanGold.png')
     window.destroy()
-    
+
 def DClean():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/CleanGoldDark.png')
     window.destroy()
     print('Reboot to finish the installation.')
-    
+
 def Firey():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/GoldFirey.png')
     window.destroy()
     print('Reboot to finish the installation.')    
-    
+
 def DFirey():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/GoldFireyDark.png')
     window.destroy()
     print('Reboot to finish the installation.')    
+
 def Horri():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/GoldHorizon.png')
     window.destroy()
@@ -39,7 +40,7 @@ def DHorri():
     os.system('gsettings set org.gnome.desktop.background picture-uri file:$PWD/Assets/GoldHorizonDark.png')
     window.destroy()
     print('Reboot to finish the installation.')
-    
+
 clean = PhotoImage(file = 'Assets/Small/CleanGold.png')
 cleanB = Button(window, image = clean, command = Clean).grid(column = 1, row = 1)
 
@@ -57,6 +58,8 @@ HorrB = Button(window, image = Horr, command = Horri).grid(column = 3, row = 1)
 
 HorrD = PhotoImage(file = 'Assets/Small/GoldHorizonDark.png')
 HorrDB = Button(window, image = HorrD, command = DHorri).grid(column = 3, row = 2)
+
+
 
 window['bg'] = '#FFBF00'
 window.title('Choose BG')
